5fbcb35d74bd181ca16d36b399f26ac40976f4df
Muhammad Nabil <nmuhd1444@gmail.com>
2026-07-30 01:57:56 +0800

    update server.js

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const app = express();
 const port = process.env.PORT || 80;
 
-// Serve static file (index.html)
-app.use(express.static(__dirname));
+// Serve fail-fail statik
+app.use(express.static(path.join(__dirname)));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    const indexPath = path.join(__dirname, 'index.html');
+    
+    // Semak jika index.html wujud
+    if (fs.existsSync(indexPath)) {
+        res.sendFile(indexPath);
+    } else {
+        res.send('<h1>🚀 Server Node.js Berjalan!</h1><p>Fail index.html belum di-copy ke folder /app.</p>');
+    }
 });
 
 app.listen(port, () => {
-    console.log(`Server Node.js berjalan di port ${port}`);
+    console.log(`Server berjalan di port ${port}`);
 });
